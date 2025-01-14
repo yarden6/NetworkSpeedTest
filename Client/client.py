867433b730c510bb3580bad9437ccf0263068c3a
yardenl <yardenl@wix.com>
2025-01-14 18:21:25 +0200

    add support in server payload packet

--- Client/client.py
+++ Client/client.py
@@ -9,6 +9,11 @@ UDP_BROADCAST_PORT = 12345
 UDP_PORT = 13117
 TCP_PORT = 7490
 MAGIC_COOKIE = b"\xab\xcd\xdc\xba"  # Magic cookie for packet format
+MESSAGE_TYPE_OFFER = 0x2
+MESSAGE_TYPE_REQUEST = 0x3
+MESSAGE_TYPE_PAYLOAD = 0x4
+
+MESSAGE_SIZE_PAYLOAD = 5
 
 
 class Client:
@@ -17,9 +22,9 @@ class Client:
         self.tcp_connections = None
         self.udp_connections = None
         self.udp_socket = None
-        self.server_udp_port = None
-        self.server_tcp_port = None
-        self.server_address = None
+        self.server_UDP_port = None
+        self.server_TCP_port = None
+        self.server_IP_address = None
         self.states = {"Startup": 0,
                        "Looking for a server": 1, "Speed test": 2}
         self.debug = True
@@ -27,13 +32,15 @@ class Client:
     def start_client(self):
         self.startup()
         self.create_udp_socket()  # Create the UDP socket before looking for a server
-        try:
-            self.looking_for_server()
-        except KeyboardInterrupt:
-            if self.debug:
-                print("DEBUG-----Exiting...")
-            self.udp_socket.close()
-            return
+        while True:
+            try:
+                if self.states == "Looking for a server":
+                    self.looking_for_server()
+            except KeyboardInterrupt:
+                if self.debug:
+                    print("DEBUG-----Exiting...")
+                self.udp_socket.close()
+                return
 
     def startup(self):
         self.client_state = self.states["Startup"]
@@ -52,24 +59,23 @@ class Client:
                 if self.file_size <= 0 or self.tcp_connections <= 0 or self.udp_connections <= 0:
                     raise ValueError("Please enter positive values.")
 
+                self.client_state = self.states["Looking for a server"]
                 break  # Exit the loop if everything is correct
 
             except ValueError as e:
                 print(f"Invalid input: {e}. Please try again.")
 
     def looking_for_server(self):
-        self.client_state = self.states["Looking for a server"]
         if self.debug:
             print(
                 f"{Fore.GREEN}DEBUG-----state: {self.client_state} {Style.RESET_ALL}")
 
-        wait_for_offer = True
-        while wait_for_offer:
+        while self.states == "Looking for a server":
             try:
                 print(f"Client started, listening for offer requests...")
                 data, address = self.udp_socket.recvfrom(1024)
-                wait_for_offer = False
-                self.handle_received_data(data, address)
+                self.states = self.states["speed test"]
+                self.handle_offer_packet(data, address)
 
             except KeyboardInterrupt:
                 print("Exiting...")
@@ -77,50 +83,66 @@ class Client:
                 self.udp_socket.close()
                 return
 
-    def handle_received_data(self, data, address):
+    def handle_payload_packet(self, data):
+        if data[:4] == MAGIC_COOKIE and data[4] == MESSAGE_TYPE_PAYLOAD:
+            total_seg_count = int.from_bytes(data[5:13], 'big')
+            curr_seg_count = int.from_bytes(data[13:21], 'big')
+            payload = data[21:].decode()
+            print(total_seg_count, curr_seg_count, payload)
+            return total_seg_count, curr_seg_count, payload
+
+    def handle_offer_packet(self, data, address):
         if self.debug:
             print(
-                f"{Fore.GREEN}DEBUG-----Received message: {data[:10]} {Style.RESET_ALL}")
-            print(f"{Fore.GREEN}DEBUG-----From address: {address} {Style.RESET_ALL}")
-        if data[:4] == MAGIC_COOKIE and data[4] == 0x2:
-            self.handle_offer(data, address)
+                f"{Fore.GREEN}DEBUG-----Received offer: {data[:10]} From: {address[0]} {Style.RESET_ALL}")
+        if data[:4] == MAGIC_COOKIE and data[4] == MESSAGE_TYPE_OFFER:
+            self.server_UDP_port = int.from_bytes(data[5:7], 'big')
+            self.server_TCP_port = int.from_bytes(data[7:9], 'big')
+            self.server_IP_address = address[0]
+            print(f"Received offer from {self.server_IP_address}")
 
-    def handle_offer(self, data, address):
-        self.server_udp_port = int.from_bytes(data[5:7], 'big')
-        self.server_tcp_port = int.from_bytes(data[7:9], 'big')
-        print(f"Received offer from {address} with UDP port {
-              self.server_udp_port} and TCP port {self.server_tcp_port}")
-        print(f"Received offer from {address}")
-        # Update self.server_address with correct TCP port and not UDP port
-        self.server_address = (address[0], self.server_tcp_port)
-        self.request_file()  # Proceed to request file
+            self.client_state = self.states["Speed test"]
+            if self.debug:
+                print(
+                    f"{Fore.GREEN}DEBUG-----state: {self.client_state} {Style.RESET_ALL}")
+            self.request_file()  # Proceed to request file
 
     def request_file(self):
         request_packet = MAGIC_COOKIE + \
-            bytes([0x3]) + self.file_size.to_bytes(8, 'big')
-
-        print(request_packet)
+            bytes([MESSAGE_TYPE_REQUEST]) + self.file_size.to_bytes(8, 'big')
+        if self.debug:
+            print(
+                f"{Fore.GREEN}DEBUG-----Request packet : {request_packet} {Style.RESET_ALL}")
 
         # Start the requests in separate threads
-        for _ in range(self.tcp_connections):
+        for i in range(self.tcp_connections):
             threading.Thread(target=self.request_tcp).start()
+            if self.debug:
+                print(
+                    f"{Fore.CYAN}DEBUG-----Sent request packet (TCP #{i}) {Style.RESET_ALL}")
 
-        for _ in range(self.udp_connections):
+        for i in range(self.udp_connections):
             threading.Thread(target=self.request_udp,
                              args=(request_packet,)).start()
+            if self.debug:
+                print(
+                    f"{Fore.MAGENTA}DEBUG-----Sent request packet (UDP #{i}) {Style.RESET_ALL}")
 
     def request_tcp(self):
         try:
-            if self.debug:
-                print(
-                    f"{Fore.GREEN}DEBUG-----Connecting to server via TCP... {Style.RESET_ALL}")
             # Create a new TCP socket per thread
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
-                tcp_socket.connect(self.server_address)
+                tcp_socket.connect(
+                    (self.server_IP_address, self.server_TCP_port))
                 tcp_socket.sendall(f"{self.file_size}\n".encode())
+                start_time = time.time()
+
                 data_received = tcp_socket.recv(self.file_size)
-                print(f"TCP transfer finished: received {
-                      len(data_received)} bytes")
+                finish_time = time.time()
+
+                total_time = finish_time - start_time
+                return len(data_received), total_time
+
         except ConnectionRefusedError:
             print("Connection refused, server may not be available")
         except Exception as e:
@@ -128,32 +150,42 @@ class Client:
 
     def request_udp(self, request_packet):
         try:
-            if self.debug:
-                print(
-                    f"{Fore.GREEN}DEBUG-----Connecting to server via UDP... {Style.RESET_ALL}")
             # Create a new UDP socket for sending
-            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-                udp_socket.sendto(request_packet, self.server_address)
-                # Wait for UDP response
-                udp_socket.settimeout(1.0)  # Set a timeout for receiving data
-                total_bytes_received = 0
-                start_time = time.time()
+            udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udp_socket.bind(("", 0))  # Bind to any available port
+            # udp_socket.settimeout(1.0)  # Set a timeout for receiving data
+            udp_socket.sendto(
+                request_packet, (self.server_IP_address, self.server_UDP_port))
 
-                while True:
-                    try:
-                        packet, _ = udp_socket.recvfrom(1024)
-                        total_bytes_received += len(packet)
-                        if len(packet) < 1024:  # End of transmission
-                            break
-                    except socket.timeout:
-                        break  # Exit on timeout
+            start_time = time.time()
 
-                end_time = time.time()
-                total_time = end_time - start_time
-                print(f"UDP transfer finished: received {
-                      total_bytes_received} bytes in {total_time:.2f} seconds")
+            total_bytes_received = 0
+            recieved_seg_count = 0
+
+            while True:
+                try:
+                    packet, _ = udp_socket.recvfrom(1024)
+                    if self.debug:
+                        print(
+                            f"{Fore.LIGHTCYAN_EX}DEBUG-----UDP Received payload packet: {packet} {Style.RESET_ALL}")
+                    total_seg_count, curr_seg_count, payload = self.handle_payload_packet(
+                        packet)
+                    print(f"{Fore.LIGHTCYAN_EX} Received segment {
+                        curr_seg_count}/{total_seg_count} {Style.RESET_ALL}")
+                    recieved_seg_count += 1
+                    total_bytes_received += len(payload)
+                    if len(packet) < 1024:  # End of transmission
+                        break
+                # except socket.timeout:
+                #     break  # Exit on timeout
+                except ValueError as e:
+                    print(f"Corrupted Message: {e}")
+
+            finish_time = time.time()
+            total_time = finish_time - start_time
+            return total_bytes_received, total_time, recieved_seg_count, total_seg_count
         except Exception as e:
-            print(f"Error sending UDP request: {e}")
+            print(f"Error during UDP request: {e}")
 
     def create_udp_socket(self):
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
--- Server/server.py
+++ Server/server.py
@@ -3,12 +3,17 @@ import struct
 import threading
 import time
 from colorama import Fore, Style
+import math
 
 # Server settings
 UDP_PORT = 13117
 TCP_PORT = 7490
 MAGIC_COOKIE = b"\xab\xcd\xdc\xba"  # Magic cookie for packet format
 MESSAGE_TYPE_OFFER = 0x2
+MESSAGE_TYPE_REQUEST = 0x3
+MESSAGE_TYPE_PAYLOAD = 0x4
+
+MESSAGE_SIZE_PAYLOAD = 5
 
 
 class Server:
@@ -26,59 +31,65 @@ class Server:
             socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_broadcast_server.setsockopt(
             socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        # self.udp_server.bind((self.ip_address, self.udp_port))
 
         broadcasting_thread = threading.Thread(
             target=self.send_broadcast_offer)
-        broadcasting_thread.start()
 
         tcp_thread = threading.Thread(target=self.handle_tcp_request)
-        tcp_thread.start()
 
         udp_thread = threading.Thread(target=self.handle_udp_request)
+
+        tcp_thread.start()
         udp_thread.start()
+        broadcasting_thread.start()
+
+        tcp_thread.join()
+        udp_thread.join()
+        broadcasting_thread.join()
 
     def handle_udp_request(self):
         udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udp_server.bind((self.ip_address, self.udp_port))
+        if self.debug:
+            print(
+                f"{Fore.LIGHTMAGENTA_EX}DEBUG-----UDP server started on port {self.udp_port} {self.ip_address}{Style.RESET_ALL}")
+
         while True:
             try:
                 data, address = udp_server.recvfrom(1024)
-                if self.debug:
-                    print(
-                        f"{Fore.YELLOW}DEBUG-----Received data: {data}, from {address} {Style.RESET_ALL}")
-                if data[:4] == MAGIC_COOKIE and data[4] == MESSAGE_TYPE_OFFER:
-                    threading.Thread(target=self.handle_udp_client_connection,
+                if data[:4] == MAGIC_COOKIE and data[4] == MESSAGE_TYPE_REQUEST:
+                    threading.Thread(target=self.process_udp_request,
                                      args=(data, address), daemon=True).start()
             except Exception as e:
                 print(f"An error occurred in UDP handling: {e}")
 
-    def handle_udp_client_connection(self, data, address):
+    def process_udp_request(self, data, address):
+        if self.debug:
+            print(
+                f"{Fore.LIGHTMAGENTA_EX}DEBUG-----UDP Recieved request: {data} {Style.RESET_ALL}")
+
         # Handle UDP connection
         try:
-            if data[:4] == MAGIC_COOKIE and data[4] == 0x3:
-                file_size = int.from_bytes(data[5:11], 'big')
+            file_size = int.from_bytes(data[5:13], 'big')
+            self.send_payload(file_size, address)
 
-            # Mock data based on size
-            file_data = b'\x00' * file_size
-            address.sendall(file_data)  # Sending mock data
         except Exception as e:
-            print(f"Error handling client: {str(e)}")
+            print(f"Error handling UDP client: {str(e)}")
 
     def handle_tcp_request(self):
         tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp_server.bind((self.ip_address, self.tcp_port))
         if self.debug:
             print(
-                f"{Fore.YELLOW}DEBUG-----TCP server started on port {self.tcp_port} {self.ip_address}{Style.RESET_ALL}")
+                f"{Fore.LIGHTBLUE_EX}DEBUG-----TCP server started on port {self.tcp_port} {self.ip_address}{Style.RESET_ALL}")
         tcp_server.listen(5)
         while True:
             try:
                 client_socket, client_address = tcp_server.accept()
                 if self.debug:
                     print(
-                        f"{Fore.YELLOW}DEBUG-----Accepted connection from {client_address}{Style.RESET_ALL}")
-                threading.Thread(target=self.handle_tcp_client_connection,
+                        f"{Fore.LIGHTBLUE_EX}DEBUG-----TCP Accepted connection from {client_address}{Style.RESET_ALL}")
+                threading.Thread(target=self.process_tcp_request,
                                  args=(client_socket,), daemon=True).start()
             except KeyboardInterrupt:
                 break
@@ -86,16 +97,30 @@ class Server:
                 print(f"An error occurred during TCP handling: {e}")
         tcp_server.close()
 
-    def handle_tcp_client_connection(self, client_socket):
+    def process_tcp_request(self, client_socket):
         # Handle TCP connection
         try:
             # Example: receive data from the client
             data = client_socket.recv(1024)
+            if self.debug:
+                print(
+                    f"{Fore.LIGHTBLUE_EX}DEBUG-----TCP Recieved request: {data} {Style.RESET_ALL}")
+
+            if data[-1] == '\n':
+                print(f"{Fore.RED} Received invalid data {
+                      data} {Style.RESET_ALL}")
             # Mock data based on size
-            file_data = b'\x00' * int(data.decode().strip())
-            client_socket.sendall(file_data)  # Sending mock data
+            file_size = int(data.decode().strip())
+
+            payload_message = ("a" * file_size).encode()
+            client_socket.sendall(payload_message)
+
+            if self.debug:
+                print(
+                    f"{Fore.LIGHTBLUE_EX}DEBUG-----TCP Sent payload: {payload_message} {Style.RESET_ALL}")
+
         except Exception as e:
-            print(f"Error handling client: {str(e)}")
+            print(f"Error handling TCP client: {str(e)}")
         finally:
             client_socket.close()  # Close the client connection
 
@@ -103,11 +128,6 @@ class Server:
         header = struct.pack('4sB', MAGIC_COOKIE, MESSAGE_TYPE_OFFER)
         message = struct.pack('>HH', self.udp_port, self.tcp_port)
         return header + message
-
-        # udp_port_bytes = struct.pack('!H', self.udp_port)
-        # tcp_port_bytes = struct.pack('!H', self.tcp_port)
-        # return MAGIC_COOKIE + \
-        #     bytes([MESSAGE_TYPE_OFFER]) + udp_port_bytes + tcp_port_bytes
 
     def send_broadcast_offer(self):
         try:
@@ -122,18 +142,32 @@ class Server:
         except KeyboardInterrupt:
             return
 
-    def build_payload_packet(self):
-        header = struct.pack('4sB', MAGIC_COOKIE, 0x4)
-        message = struct.pack('>HH', self.udp_port, self.tcp_port)
-        return header + message
+    def build_payload_packet(self, total_seg_count: int, curr_seg_count: int, payload):
+        header = struct.pack('4sB', MAGIC_COOKIE, MESSAGE_TYPE_PAYLOAD)
+        total_seg_count = total_seg_count.to_bytes(8, 'big')
+        curr_seg_count = curr_seg_count.to_bytes(8, 'big')
+        return header + total_seg_count + curr_seg_count + payload
 
-    def send_payload(self):
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+    def send_payload(self, file_size, address):
         try:
-            # Create the payload message
-            payload_packet = self.build_offer_packet()
+            udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            total_seg_count = math.ceil(file_size / MESSAGE_SIZE_PAYLOAD)
+            for i in range(total_seg_count):
+                curr_seg_count = i
+                offset = curr_seg_count * MESSAGE_SIZE_PAYLOAD
+                curr_seg_size = min(MESSAGE_SIZE_PAYLOAD, file_size - offset)
+                curr_seg_data = ("a" * curr_seg_size).encode()
+                payload_packet = self.build_payload_packet(
+                    total_seg_count, i, curr_seg_data)
 
+                try:
+                    udp_socket.sendto(payload_packet, address)
+                    if self.debug:
+                        print(
+                            f"{Fore.LIGHTMAGENTA_EX}DEBUG-----Sent payload packet #{i} via UDP: {payload_packet} {Style.RESET_ALL}")
+                except Exception as e:
+                    print(
+                        f"An error occurred while sending payload: {str(e)}")
         except OSError:
             return
         except KeyboardInterrupt:
